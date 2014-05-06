0d563373060207091e86a3a7bb9b29553dcab353
Pedro Howat <pedro.howat@gmail.com>
2014-05-06 00:19:46 -0300

    Refactoring our --cli loop

--- uowm.py
+++ uowm.py
@@ -9,8 +9,6 @@ from multiprocessing import Process, Value
 from time import sleep, time
 
 options = {}
-loop_proc = None
-last_change_ts = Value('i', 0)
 
 """ --- TODO --- 
 * Tornado server
@@ -20,26 +18,53 @@ last_change_ts = Value('i', 0)
 * grade wallpapers
 """
 
-def change_wallpaper_loop(sleep_secs, wp_dirs, last_change_ts):
-    sleep_secs = int(sleep_secs)
-    while 1:
-        now = int(time())
-        if now - last_change_ts.value >= sleep_secs:
-            change_wallpaper(wp_dirs)
-            last_change_ts.value = now
-        sleep(sleep_secs)
-        
-def terminate_wallpaper_loop():
-    global loop_proc
-    if loop_proc is not None:
-        loop_proc.terminate()
+class WPCmd(object):
 
-def cmd_change(split_args, directories):
-    global last_change_ts
-    dirs = split_args[1:] if len(split_args) > 1 else directories
-    winner = change_wallpaper(dirs)
-    last_change_ts.value = int(time()) 
-    print winner
+    def __init__(self, directories):
+        self.directories = directories
+        self.loop_proc = None
+        self.last_change_ts = Value('i', 0)
+
+    @staticmethod
+    def change_wallpaper_loop(sleep_secs, wp_dirs, last_change_ts):
+
+        sleep_secs = int(sleep_secs)
+        while 1:
+            now = int(time())
+            if now - last_change_ts.value >= sleep_secs:
+                change_wallpaper(wp_dirs)
+                last_change_ts.value = now
+            sleep(sleep_secs)
+            
+    def __terminate_wallpaper_loop(self):
+        if self.loop_proc is not None:
+            self.loop_proc.terminate()
+
+    def change(self, split_args):
+        dirs = split_args[1:] if len(split_args) > 1 else self.directories
+        winner = change_wallpaper(dirs)
+        self.last_change_ts.value = int(time()) 
+        print winner
+    
+    def startloop(self, split_args):
+        if len(split_args) > 1:
+            sleep_secs = split_args[1]
+        else:
+            sleep_secs = 30
+        print "Changing wallpaper every "+str(sleep_secs)+" seconds."
+        self.loop_proc = Process(target=WPCmd.change_wallpaper_loop, 
+                                 args=(sleep_secs, self.directories,
+                                       self.last_change_ts))
+        self.loop_proc.start()
+
+    def endloop(self, split_args):
+        print "Stopped automatic wallpaper change."
+        self.__terminate_wallpaper_loop()
+
+    def exit(self, split_args):
+        print "So long and thanks for all the fish."
+        self.__terminate_wallpaper_loop()
+        sys.exit()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -57,36 +82,19 @@ if __name__ == '__main__':
         main()
     elif options.cli is True:
         prev_cmd = ""
+        cmd = WPCmd(options.directories)
         while 1:
-            cmd = raw_input("> ")
-            if cmd.strip() == "":
-                cmd = prev_cmd
+            cur_cmd = raw_input("> ")
+            if cur_cmd.strip() == "":
+                cur_cmd = prev_cmd
+            split_args = cur_cmd.split(' ')
 
-            split_args = cmd.split(' ')
-            if split_args[0] == "change":
-                prev_cmd = cmd
-                cmd_change(split_args, options.directories)
-
-            elif split_args[0] == "startloop":
-                prev_cmd = cmd
-                if len(split_args) > 1:
-                    sleep_secs = split_args[1]
-                else:
-                    sleep_secs = 30
-                print "Changing wallpaper every "+str(sleep_secs)+" seconds."
-                loop_proc = Process(target=change_wallpaper_loop, 
-                                    args=(sleep_secs, options.directories,
-                                        last_change_ts))
-                loop_proc.start()
-            elif split_args[0] == "endloop":
-                prev_cmd = cmd
-                print "Stopped automatic wallpaper change."
-                terminate_wallpaper_loop()
-            elif split_args[0] == "exit":
-                print "So long and thanks for all the fish."
-                terminate_wallpaper_loop()
-                sys.exit()
+            try:
+                cmd_func = getattr(cmd, split_args[0])
+            except AttributeError:
+                print "Don't know how to do ["+str(cur_cmd)+"]."
             else:
-                print "Don't know how to do ["+str(cmd)+"]."
+                prev_cmd = cur_cmd
+                cmd_func(split_args)
     else:
         change_wallpaper(options.directories)
