f9e3bba88dc795ad307d4ee79e6be32ef3f651c4
Pedro Howat <pedro.howat@gmail.com>
2014-05-07 11:18:50 -0300

    WPConfiguration tests.

--- t.py
+++ t.py
@@ -1,4 +1,5 @@
-from uowmlib import WPCollection, WPBackendGsettings
+from uowmlib import WPCollection, WPConfiguration
+from uowmbackends import WPBackendGsettings
 import unittest
 from shell import shell
 
@@ -25,6 +26,28 @@ class TestGsettings(unittest.TestCase):
             "/usr/bin/env gsettings get org.gnome.desktop.background \
             picture-uri").output()[0]
         self.assertEqual("'file://"+winner+"'", picture_uri)
+
+class TestConfiguration(unittest.TestCase):
+    def setUp(self):
+        self._conf = WPConfiguration("./uowmrc-example")
+    
+    def test_default_dirs(self):
+        self.assertEqual(self._conf.default_dirs, ['/home/pedro/wallpapers'])
+
+    def test_append_default_dirs(self):
+        self.assertFalse(self._conf.append_default_dirs)
+
+    def test_log_file(self):
+        self.assertEqual(self._conf.log_file, '/home/pedro/.uowm/log')
+
+    def test_no_repeat(self):
+        self.assertEqual(self._conf.no_repeat, 20)
+
+    def test_cycle_dirs(self):
+        self.assertTrue(self._conf.cycle_dirs)
+
+    def test_backend(self):
+        self.assertEqual(self._conf.backend,  "Noop")
 
 if __name__ == '__main__':
     unittest.main()
