c01675f603cd35b74cbb1bb98ee06de2a8c1172b
Pedro Howat <pedro.howat@gmail.com>
2014-05-13 22:37:54 -0300

    Silly ascii-art intro

--- uowmconsole.py
+++ uowmconsole.py
@@ -12,6 +12,19 @@ import sys
 class WPConsole(cmd.Cmd):
     def __init__(self, dirs=[]):
         cmd.Cmd.__init__(self)
+        self.prompt = ">> "
+        self.intro = """
+  888     888 .d88888b. 888       888888b     d888 
+  888     888d88P" "Y88b888   o   8888888b   d8888 
+  888     888888     888888  d8b  88888888b.d88888 
+  888     888888     888888 d888b 888888Y88888P888 
+  888     888888     888888d88888b888888 Y888P 888 
+  888     888888     88888888P Y88888888  Y8P  888 
+  Y88b. .d88PY88b. .d88P8888P   Y8888888   "   888 
+   "Y88888P"  "Y88888P" 888P     Y888888       888 
+   
+     Unecessarily Overengineed Wallpaper Manager
+        """
         self.wpcmd = WPCmd(dirs)
 
     def do_change(self, args):
