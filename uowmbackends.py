09f0c67610223f21981e7a89262bbf3bd12019ba
Pedro Howat <pedro.howat@gmail.com>
2014-05-19 19:39:38 -0300

    os.system does not like unicode strings

--- uowmbackends.py
+++ uowmbackends.py
@@ -1,31 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
+import subprocess
 
 class WPBackendGsettings(object):
     def set_wallpaper(self, wpaper):
         # In case you don't have DBUS_SESSION_BUS_ADDRESS and/or
         # DISPLAY set. export it before running this script. 
-        cmd = """\
-/usr/bin/env gsettings set \
-org.gnome.desktop.background picture-uri "file://{0}"\
-        """.format(wpaper)
-        os.system(cmd)
+
+        cmd = ['/usr/bin/env', 'gsettings', 'set', 
+               'org.gnome.desktop.background', 'picture-uri', 
+               '"file://'+wpaper+'"']
+        subprocess.call(cmd)
         
 class WPBackendMate(object):
     def set_wallpaper(self, wpaper):
 
-        cmd = """\
-/usr/bin/env mateconftool-2 --set --type=string \
-/desktop/mate/background/picture_filename "file://{0}"\
-        """.format(wpaper)
-        os.system(cmd)
+        cmd = ['/usr/bin/env', 'mateconftool-2', '--set', '--type=string', 
+               '/desktop/mate/background/picture_filename',
+               '"file://'+wpaper+'"']
+        subprocess.call(cmd)
         
 class WPBackendXmonad(object):
     # Through feh.
         def set_wallpaper(self, wpaper):
-            os.system("/usr/bin/feh --bg-max "+wpaper);
+            subprocess.call(['/usr/bin/feh', '--bg-max', wpaper])
 
 class WPBackendNoop(object):
     def set_wallpaper(self, wpaper):
