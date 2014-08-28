8ea152dcf055ef7b9340d2e44d17da3949fa57fa
Pedro Howat <pedro.howat@gmail.com>
2014-08-28 00:44:14 -0300

    preparing the json structure i'll store in rethinkdb

--- uowmcolletor.py
+++ uowmcolletor.py
@@ -3,13 +3,31 @@
 
 from os import walk
 import pprint
+import time
 
 def list_files(basedir):
     paths = []
     for dirpath,dirname,filenames in walk(basedir):
-        paths += map(lambda x: (dirpath+"/"+x).replace(basedir,''), filenames)
+        paths += map(lambda x: [dirpath, x], filenames)
     return paths
 
+def extract_tags(basedir, path):
+    return filter(lambda x: len(x) > 0, path.replace(basedir,'').split("/"))
+
+def create_structure(basedir, path):
+    return {
+        'fullpath': path[0]+'/'+path[1],
+        'name': path[1],
+        'tags': extract_tags(basedir, path[0]),
+        'added': int(time.time()),
+        'last_display': 0,
+    }
+
+def create_structures(basedir, paths):
+    return map(lambda x: create_structure(basedir, x), paths)
+
 if __name__ == '__main__':
-    paths = list_files("/home/pedro/wallpapers")
-    pprint.pprint(paths)
+    basedir = "/home/pedro/wallpapers";
+    paths = list_files(basedir)
+    db_struct = create_structures(basedir, paths)
+    pprint.pprint(db_struct)
