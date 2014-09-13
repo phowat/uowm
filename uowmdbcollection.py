616f30dd281c2248068706c5b55bc63cea706756
Pedro Howat <pedro.howat@gmail.com>
2014-09-13 20:46:12 -0300

    Preparing db collection to substitue WPCollection

--- uowmdbcollection.py
+++ uowmdbcollection.py
@@ -2,10 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import rethinkdb as r
+import random
+
+
+#TODO: 
+# Multiple tags and 
+# Multiple tags or
+# Display new ones( without a last display timestamp) 
 
 if __name__ == '__main__':
     _conn = r.connect(db='uowm')
     cur = r.table('wallpapers').\
           filter(r.row['tags'].contains("earthporn")).run(_conn)
+    collection = []
     for cu in cur:
-        print cu
+        collection.append(cu)
+    print random.choice(collection)
